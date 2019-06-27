8558132dac72f4f3d747ecfe868a279d724ede6a
wngh0908 <38426656+wngh0908@users.noreply.github.com>
2019-06-27 21:52:17 +0900

    View 초안

--- WireShark/src/View/MainView.java
+++ WireShark/src/View/MainView.java
@@ -1,6 +1,5 @@
 package View;
 
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,40 +13,51 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class MainView extends JFrame implements ActionListener, Runnable{
+public class MainView extends JFrame implements ActionListener, Runnable {
 
 	public JTextField text_filter;
-	public JPanel pan_fst, pan_sec, pan_thd, pan_four, pan_fiv, pan_six;
-	public JButton  btn_run= new JButton("RUN");
-	public JButton btn_stop= new JButton("STOP"); 
+	public JPanel upper_pan, lower_pan, Main_pan, pan_fst, pan_btn, pan_filter, pan_packetTable, pan_PacketInfo, pan_PacketHexCode;
+	public JButton btn_run = new JButton("RUN");
+	public JButton btn_stop = new JButton("STOP");
 	public JLabel la_filter;
 	public JMenuBar menubar;
-	public JMenu file, edit, view, go;	
-	
+	public JMenu file, edit, view, go;
+	public JTable table;
+
 	public JTextArea area_packinfo;
 	public JScrollPane scrol_packinfo;
 	/**
 	 * Create the frame.
 	 */
+
+	// packet_table을 텍스트 필드에서 JTable로 바꿀까하는 실험하는 중
+	String title[] = { "No.", "Time", "Source", "Destination", "Protocol", "Length", "Internet Protocol" };
+	String data[][];
+	//
 	
-	Scanner in;  
-	
+	Scanner in;
+
 	public MainView() {
-		
+
 		// JFrame
+		Main_pan = new JPanel(new GridLayout(3, 1));
+		// 하나의 Main 패널 안에 upper_pan, packet_table, lower_pan이 담김
+		upper_pan = new JPanel(new GridLayout(3, 1));
+		lower_pan = new JPanel(new GridLayout(2, 1));
+
 		setResizable(false); // frame 크기 조절
 		setTitle("WireShark"); // frame title
-		//getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER,0,30));
-		getContentPane().setLayout(new BorderLayout(10,30));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 종료 버튼
-		setBounds(300, 100, 800, 600);   
-		// JPanel
-		// 1.메뉴바 
+		setBounds(300, 100, 800, 600); // wireshark 사이즈
+
+		// 1.메뉴바
 		menubar = new JMenuBar();
-		
+
+		// file 메뉴와 아이템 객체들
 		file = new JMenu("file");
 		JMenuItem open = new JMenuItem("open");
 		JMenuItem close = new JMenuItem("close");
@@ -55,7 +65,8 @@ public class MainView extends JFrame implements ActionListener, Runnable{
 		file.add(open);
 		file.add(close);
 		file.add(save);
-		
+
+		// edit 메뉴와 아이템 객체들
 		edit = new JMenu("edit");
 		JMenuItem FindPacket = new JMenuItem("FindPacket");
 		JMenuItem MarkUnMark = new JMenuItem("Mark / UnMark");
@@ -63,11 +74,13 @@ public class MainView extends JFrame implements ActionListener, Runnable{
 		edit.add(FindPacket);
 		edit.add(MarkUnMark);
 		edit.add(Ignore);
-		
+
+		// view 메뉴와 아이템 객체들
 		view = new JMenu("view");
 		JMenuItem ShowPacketinNewWindow = new JMenuItem("Show Packet in New Window");
 		view.add(ShowPacketinNewWindow);
-		
+
+		// go 메뉴와 아이템 객체들
 		go = new JMenu("go");
 		JMenuItem Gotopacket = new JMenuItem("Go to packet");
 		JMenuItem Previouspacket = new JMenuItem("Previous packet");
@@ -79,94 +92,98 @@ public class MainView extends JFrame implements ActionListener, Runnable{
 		go.add(Nextpacket);
 		go.add(Firstpacket);
 		go.add(Lastpacket);
-		
-		
-		//pan_fst.setLayout(new GridLayout(1,4));
-		
+
+		// 메뉴바에 위의 메뉴들을 전부 붙임.
 		menubar.add(file);
 		menubar.add(edit);
 		menubar.add(view);
 		menubar.add(go);
-		
-		setJMenuBar(menubar);
-		
-		// 2. 실행 및 정지 버튼 담을 판넬
-		//setContentPane(pan_sec);
-		pan_sec = new JPanel();
-		btn_run= new JButton("RUN");
-		btn_stop= new JButton("STOP");
-		pan_sec.add(btn_run);
-		pan_sec.add(btn_stop);
-		
 
-	
+		// Frame에 메뉴바 추가
+		setJMenuBar(menubar);
+
 		
-		// 3. 필터 담을 판넬(보류)
-		pan_thd = new JPanel();
-		text_filter = new JTextField(10);
-		la_filter = new JLabel("filter : ");
-		pan_thd.add(la_filter);
-		pan_thd.add(text_filter);
-		//setContentPane(pan_thd);
-		//pan_thd.setVisible(true);
+		// 2. upper_pan에 추가할 3가지 판넬
+			// pan_btn : 실행 및 정지 버튼 담을 판넬
+			// pan_filter : 검색을 위한 filter 판넬
+			// pan_packetInfo : Packet의 정보를 나열하는 헤더가 담긴 판넬 -> 따로 객체 안만듦, add(new ~)로 바로 생성함
+		pan_btn = new JPanel();
+		// pan_sec.setBackground(Color.blue);
+		btn_run = new JButton("RUN");
+		btn_stop = new JButton("STOP");
+		pan_btn.add(btn_run);
+		pan_btn.add(btn_stop);
+
+		pan_filter = new JPanel();
+		// pan_thd.setBackground(Color.red);
+		text_filter = new JTextField(20);
+		la_filter = new JLabel("  filter : ");
+		pan_filter.add(la_filter);
+		pan_filter.add(text_filter);
+
+		// 3. 캡처된 패킷의 자세한 정보 요약하는 판넬
+		pan_packetTable = new JPanel();
+		// pan_packetTable.setBackground(Color.yellow);
+		pan_packetTable.setLayout(new GridLayout(1, 1));
+		area_packinfo = new JTextArea(); 
+		scrol_packinfo = new JScrollPane(area_packinfo);
+		pan_packetTable.add(scrol_packinfo);
 		
-		
-		// 4. 캡처된 패킷의 자세한 정보 요약하는 판넬
-		
-		
-		pan_four = new JPanel();
-		pan_four.setLayout(new GridLayout(6,1));
+		// 실험중인 테이블화
+		table = new JTable(data, title);
+
+		// 4.lower 테이블
+			// pan_PacketInfo : 해당 패킷에 대한 구체적인 정보 출력 
+			// pan_PacketHexCode : 해당 패킷에 대한 헥사 값 출력 
+		pan_PacketInfo = new JPanel();
 		area_packinfo = new JTextArea();
 		scrol_packinfo = new JScrollPane(area_packinfo);
-		scrol_packinfo.setBounds(300, 500, 800, 200);
-		pan_four.add(new JLabel("    No.                  Time                Source                                         Destination                    Protocol                          Inof"));
-		pan_four.add(scrol_packinfo);
-		//pan_four.add(area_packinfo);
-		//setContentPane(pan_four);
-		//pan_four.setBounds(300, 500, 800, 200);
-		//pan_four.set
-		
-		// 5. 4번에 나아가 사에한 정보 표시하는 판넬
-		pan_fiv = new JPanel();
-		pan_fiv.setLayout(new GridLayout(6,1));
-		//setContentPane(pan_fiv);
-	
-		// 6. 패킷의 데이터를 헥사값으로 표시할 판넬
-		pan_six = new JPanel();
-		pan_six.setLayout(new GridLayout(6,1));
-		//setContentPane(pan_six);
-		
-		//add(pan_sec);
-		//add(pan_four);
-		//add(pan_thd);
-		//add(pan_fiv);
-		//add(pan_six);
-		add("North",pan_sec);
-		add("Center",pan_four);
-		add("South",pan_thd);
-		add("West",pan_fiv);
-		add("East",pan_six);
+		scrol_packinfo.setSize(100, 100);
+		pan_PacketInfo.add(scrol_packinfo);
+		pan_PacketInfo.setLayout(new GridLayout(1, 1));
+		// setContentPane(pan_fiv);
 
+		pan_PacketHexCode = new JPanel();
+		area_packinfo = new JTextArea();
+		scrol_packinfo = new JScrollPane(area_packinfo);
+		scrol_packinfo.setSize(100, 100);
+		pan_PacketHexCode.add(scrol_packinfo);
+		pan_PacketHexCode.setLayout(new GridLayout(1, 1));
+
+		// upper 팬
+		upper_pan.add(pan_btn);
+		upper_pan.add(pan_filter);
+		upper_pan.add(new JLabel(
+				"    No.                  Time                Source                                         Destination                    Protocol                          Inof"));
+
+		// lower 팬
+		lower_pan.add(pan_PacketInfo);
+		lower_pan.add(pan_PacketHexCode);
+
+		// 메인 팬 = uppper 팬 + 패킷 테이블 + lower 팬
+		Main_pan.add(upper_pan);
+		Main_pan.add(pan_packetTable);
+		Main_pan.add(lower_pan);
+
+		add(Main_pan);
 		setVisible(true);
 	}
+
 	@Override
 	public void run() {
 		while (true) {
-			String msg = in.nextLine(); 
-			area_packinfo.append(msg+"\n");
-			
+			String msg = in.nextLine();
+			area_packinfo.append(msg + "\n");
+
 			// 스크롤바 따라 내려오게 하려고
 			area_packinfo.setCaretPosition(area_packinfo.getText().length());
-			
 		}
 	}
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
 	public static void main(String[] args) {
 		new MainView();
 	}
